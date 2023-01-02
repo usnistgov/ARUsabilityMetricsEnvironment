5da73052c3c608ef66cb8bf2e685c8e9ece76809
James Folk <james@njligames.com>
2023-01-02 17:42:04 -0500

    Changed Scene name and added readme.

--- VS/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\NIST\ARUsabilityMetricsEnvironment\VS\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-api-debugger.cpp"
---
+++ VSProject/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -0,0 +1,4 @@
+#include "il2cpp-config.h"
+#include "C:\NIST\ARUsabilityMetricsEnvironment\VSProject\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
+#include "C:\NIST\ARUsabilityMetricsEnvironment\VSProject\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
+#include "C:\NIST\ARUsabilityMetricsEnvironment\VSProject\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
